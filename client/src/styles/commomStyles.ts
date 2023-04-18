bb6258238a1facad7e37bd6d022cf3900b851e29
choigicheol <kofpo900916@gmail.com>
2023-04-18 15:21:42 +0900

    [client] 폰트 적용, common css

---
+++ client/src/styles/commomStyles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const CenterContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ColumnContainer = styled(CenterContainer)`
+  flex-direction: column;
+`;
