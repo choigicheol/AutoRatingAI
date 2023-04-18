7d1e62cfda68addf02e6bbf0159defce89c996c3
choigicheol <kofpo900916@gmail.com>
2023-04-18 15:22:26 +0900

    [client] Header, Footer 컴포넌트

---
+++ client/src/components/Header/Header.style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+  > span {
+    font-size: 40px;
+  }
+`;
